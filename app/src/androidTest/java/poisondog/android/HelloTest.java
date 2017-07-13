5882e3b68a6439cacd6b4f5b64efb0593a0a36d6
ts0963187279 <RS Wang/cob/wae@wae>
2017-07-13 16:48:49 +0800

    upload

---
+++ app/src/androidTest/java/poisondog/android/HelloTest.java
@@ -0,0 +1,21 @@
+package poisondog.android;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class poisondog.android.HelloTest \
+ * poisondog.android.tests/android.test.InstrumentationTestRunner
+ */
+public class HelloTest extends ActivityInstrumentationTestCase2<Hello> {
+
+    public HelloTest() {
+        super("poisondog.android", Hello.class);
+    }
+
+}
